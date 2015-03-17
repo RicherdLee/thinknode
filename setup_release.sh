6186cc56ace3e7eb8735b9cf94b72c54df066567
yanglin <yanglin@enlink-mob.com>
2015-03-17 20:56:11 +0800

    fix setup

--- setup_release.sh
+++ setup_release.sh
@@ -2,15 +2,15 @@
 
 #Mysql数据库配置
 cp ./#App/Common/Conf/config.js ./#App/Common/Conf/config.js.bak
-sed -i "s/.*db_host.*/'db_host' => '192.168.0.20',/" ./#App/Common/Conf/config.js
-sed -i "s/.*db_port.*/'db_port' => '4047',/" ./#App/Common/Conf/config.js
-sed -i "s/.*db_name.*/'db_name' => 'thinknode',/" ./#App/Common/Conf/config.js
-sed -i "s/.*db_user.*/'db_user' => 'thinknode',/" ./#App/Common/Conf/config.js
-sed -i "s/.*db_pwd.*/'db_pwd' => '2014_thinknode_admin',/" ./#App/Common/Conf/config.js
+sed -i "s/.*db_host.*/db_host:'192.168.0.20',/" ./#App/Common/Conf/config.js
+sed -i "s/.*db_port.*/db_port:'4047',/" ./#App/Common/Conf/config.js
+sed -i "s/.*db_name.*/db_name:'thinknode',/" ./#App/Common/Conf/config.js
+sed -i "s/.*db_user.*/db_user:'thinknode',/" ./#App/Common/Conf/config.js
+sed -i "s/.*db_pwd.*/db_pwd:'2014_thinknode_admin',/" ./#App/Common/Conf/config.js
 
 #关闭调试
 cp ./index.js ./index.js.bak
-sed -i "s/.*THINK.APP_DEBUG.*/THINK.APP_DEBUG = true;/" ./index.js
+sed -i "s/.*THINK.APP_DEBUG.*/THINK.APP_DEBUG = false;/" ./index.js
 
 
 #清除缓存
