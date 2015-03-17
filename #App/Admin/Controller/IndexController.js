8a2a9d643dd1fac502461c23b3ada0e89bafc8b8
yanglin <yanglin@enlink-mob.com>
2015-03-17 21:14:58 +0800

    fix bug

--- #App/Admin/Controller/IndexController.js
+++ #App/Admin/Controller/IndexController.js
@@ -8,7 +8,7 @@
 module.exports = Controller("Common/AdminBaseController", function () {
     "use strict";
     return {
-        init: function () {
+        init: function (http) {
             this.super_("init", http);
         },
 
@@ -21,24 +21,24 @@ module.exports = Controller("Common/AdminBaseController", function () {
             var info = [];
             var username = '';
             return this.session("userInfo").then(function (user) {
-                return Promise.all([self.getDbVersion(),X("Admin/AdminMenu",user.id).getAdminMenu()]).then(function (data) {
-                    if(user){
+                if(user){
+                    return Promise.all([self.getDbVersion(),X("Admin/AdminMenu",user.id).getAdminMenu()]).then(function (data) {
                         username = user.nickname ? user.nickname : user.username;
-                    }
-                    var os = thinkRequire("os");
-                    info.platform = os.platform();
-                    info.nodeVersion = process.version;
-                    info.serverTime = new Date().Format('YYYY-MM-DD HH:mi:ss');
-                    info.runTime = 'Node.js';
-                    info.dbVersion = data[0][0].ver;
-                    info.version = C("app_version");
-                    info.versionCode = C("app_version_code");
+                        var os = thinkRequire("os");
+                        info.platform = os.platform();
+                        info.nodeVersion = process.version;
+                        info.serverTime = new Date().Format('YYYY-MM-DD HH:mi:ss');
+                        info.runTime = 'Node.js';
+                        info.dbVersion = data[0][0].ver;
+                        info.version = C("app_version");
+                        info.versionCode = C("app_version_code");
 
-                    self.assign("adminMenu",data[1]);
-                    self.assign("username", username);
-                    self.assign("info", info);
-                    return self.display();
-                });
+                        self.assign("adminMenu",data[1]);
+                        self.assign("username", username);
+                        self.assign("info", info);
+                        return self.display();
+                    });
+                }
             });
         },
 
