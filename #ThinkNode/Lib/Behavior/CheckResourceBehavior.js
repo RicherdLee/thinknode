2c10f801ab55733f3b452815e88907c805832e91
yanglin <yanglin@enlink-mob.com>
2015-03-27 22:31:15 +0800

    优化静态文件输出

--- #ThinkNode/Lib/Behavior/CheckResourceBehavior.js
+++ #ThinkNode/Lib/Behavior/CheckResourceBehavior.js
@@ -30,50 +30,29 @@ module.exports = Behavior(function () {
             var file = THINK.ROOT_PATH + '/' + decodeURI(pathname);
             //正则判断是否文件
             var urlReg = new RegExp(/[^\/]+\/([^\.]*)\/([^\/]+\.[^\/\.]+)$/);
-            var flag = !!file.match(urlReg);
 
-            var res = this.http.res;
-            var req = this.http.req;
-            if (flag) {
-                fs.stat(file, function (err, stats) {
-                    if (err) {
-                        res.statusCode = 404;
+            if (!!file.match(urlReg)) {
+                var res = this.http.res;
+                var req = this.http.req;
+                var contentType = mime.lookup(file);
+                res.setHeader('Content-Type', contentType + '; charset=' + C('encoding'));
+                var acceptEncoding = "Accept-Encoding".toLowerCase();
+                var fileStream = fs.createReadStream(file);
+                if ((req.headers[acceptEncoding] || '').indexOf('gzip') != -1 && contentType.match(/(javascript|css)/)) {
+                    res.setHeader('Content-Encoding', 'gzip');
+                    var gzip = fileStream.pipe(zlib.createGzip());
+                    gzip.pipe(res);
+                    gzip.on('end', function () {
                         res.end();
-                    } else {
-                        var contentType = mime.lookup(file);
-                        var exp_maxage = 30 * 24 * 3600;
-                        var expires = new Date();
-                        var lastModified = stats.mtime.toUTCString();
-                        var ifModifiedSince = "If-Modified-Since".toLowerCase();
-                        var acceptEncoding = "Accept-Encoding".toLowerCase();
-                        expires.setTime(expires.getTime() + exp_maxage * 1000);
-                        res.setHeader('Content-Type', contentType || "text/plain");
-                        res.setHeader("Cache-Control", "max-age=" + exp_maxage);
-                        res.setHeader("Expires", expires.toUTCString());
-                        res.setHeader("Last-Modified", lastModified);
-                        if (req.headers[ifModifiedSince] && lastModified == req.headers[ifModifiedSince]) {
-                            res.writeHead(304, "Not Modified");
-                            res.end();
-                        } else {
-                            var fileStream = fs.createReadStream(file);
-                            if ((req.headers[acceptEncoding] || '').indexOf('gzip') != -1 && contentType.match(/(javascript|css)/)) {
-                                res.setHeader('Content-Encoding', 'gzip');
-                                var gzip = fileStream.pipe(zlib.createGzip());
-                                gzip.pipe(res);
-                                gzip.on('end', function () {
-                                    res.end();
-                                });
-                            } else {
-                                fileStream.pipe(res);
-                                fileStream.on('end', function () {
-                                    res.end();
-                                });
-                            }
-                        }
-                    }
-                });
+                    });
+                } else {
+                    fileStream.pipe(res);
+                    fileStream.on('end', function () {
+                        res.end();
+                    });
+                }
             } else {
-                res.statusCode = 403;
+                res.statusCode = 404;
                 res.end();
             }
             //返回一个pendding promise, 不让后续执行
