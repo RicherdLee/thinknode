bb0543329ca8b86b03a34011e01a3cf7a84f8fb7
yanglin <yanglin@enlink-mob.com>
2015-03-30 13:58:57 +0800

    回归ccap

--- #App/Admin/Controller/PublicController.js
+++ #App/Admin/Controller/PublicController.js
@@ -41,57 +41,56 @@ module.exports = Controller("AppFrameController", function () {
         },
 
         captchaAction: function(){
-            //var ccap = thinkRequire("ccap")({
-            //    width: 200,
-            //    offset: 30,
-            //    _text_len: 4
-            //});
-            //var captchaPromise = getPromise(ccap.get());
-            //var self = this;
-            //return captchaPromise.then(function(data){
-            //    self.session("verify",data[0]);
-            //    self.header("Content-Type", "image/jpeg");
-            //    self.end(data[1]);
-            //});
+            var ccap = thinkRequire("ccap")({
+                width: 200,
+                offset: 30
+            });
+            var captchaPromise = getPromise(ccap.get());
+            var self = this;
+            return captchaPromise.then(function(data){
+                self.session("verify",data[0]);
+                self.header("Content-Type", "image/jpeg");
+                self.end(data[1]);
+            });
 
             //captcha
-            var captcha = require('canvas-captcha')
-                ,captchaOptions = {
-                    charPool: ('abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz'.toUpperCase() + '1234567890').split('') //char pool Array
-                    ,size: {
-                        width: 120
-                        ,height: 50
-                    } //image size
-                    ,textPos: {
-                        left: 15
-                        ,top: 40
-                    } //text drawing start position
-                    ,rotate: .01 //text ratate
-                    ,charLength: 4 //how many chars
-                    ,font: '35px Arial' //font size
-                    ,strokeStyle: '#46A3FF' //style
-                    ,bgColor: '#FFFFFF' //bg color
-                    ,confusion: true //draw another group background text to mangle the text
-                    ,cFont: '40px Arial' //bg text style
-                    ,cStrokeStyle: '#87CEEB' //bg text color
-                    ,cRotate: -.05 //bg text rotate
-                };
-
-            //use promise
-            var captchaPromise = function(options) {
-                return new Promise(function(resolve, reject) {
-                    captcha(options, function(err, data) {
-                        if(err) reject(err);
-                        else resolve(data);
-                    })
-                })
-            };
-            var self = this;
-            return captchaPromise(captchaOptions).then(function (data) {
-                self.session("verify",data.captchaStr);
-                self.header("Content-Type", "image/png");
-                self.end(data.captchaImg);
-            });
+            //var captcha = require('canvas-captcha')
+            //    ,captchaOptions = {
+            //        charPool: ('abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz'.toUpperCase() + '1234567890').split('') //char pool Array
+            //        ,size: {
+            //            width: 120
+            //            ,height: 50
+            //        } //image size
+            //        ,textPos: {
+            //            left: 15
+            //            ,top: 40
+            //        } //text drawing start position
+            //        ,rotate: .01 //text ratate
+            //        ,charLength: 4 //how many chars
+            //        ,font: '35px Arial' //font size
+            //        ,strokeStyle: '#46A3FF' //style
+            //        ,bgColor: '#FFFFFF' //bg color
+            //        ,confusion: true //draw another group background text to mangle the text
+            //        ,cFont: '40px Arial' //bg text style
+            //        ,cStrokeStyle: '#87CEEB' //bg text color
+            //        ,cRotate: -.05 //bg text rotate
+            //    };
+            //
+            ////use promise
+            //var captchaPromise = function(options) {
+            //    return new Promise(function(resolve, reject) {
+            //        captcha(options, function(err, data) {
+            //            if(err) reject(err);
+            //            else resolve(data);
+            //        })
+            //    })
+            //};
+            //var self = this;
+            //return captchaPromise(captchaOptions).then(function (data) {
+            //    self.session("verify",data.captchaStr);
+            //    self.header("Content-Type", "image/png");
+            //    self.end(data.captchaImg);
+            //});
         },
 
         errorAction: function(){
