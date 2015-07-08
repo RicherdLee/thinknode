24c797d5b138158a89ca3e41f36408613d6c1084
yanglin <yanglin@enlink-mob.com>
2015-07-08 18:34:18 +0800

    框架优化

--- index.js
+++ index.js
@@ -5,49 +5,25 @@
  * @license    MIT
  * @version    15/1/15
  */
-var fs = require('fs');
 var path = require('path');
 
-var isDir = function (p) {
-    if (!fs.existsSync(p)) {
-        return false;
-    }
-    var stats = fs.statSync(p);
-    return stats.isDirectory();
-};
-var mkdir = function (p, mode) {
-    mode = mode || '0777';
-    if (fs.existsSync(p)) {
-        chmod(p, mode);
-        return true;
-    }
-    var pp = path.dirname(p);
-    if (fs.existsSync(pp)) {
-        fs.mkdirSync(p, mode);
-    } else {
-        mkdir(pp, mode);
-        mkdir(p, mode);
-    }
-    return true;
-};
+if(!global.THINK){
+    global.THINK = [];
+}
 
 if (!THINK.ROOT_PATH) {
-    throw  new Error('ROOT_PATH must be defined');
+    throw  new Error('global.THINK.ROOT_PATH must be defined');
 }
 //静态资源目录
 if (THINK.RESOURCE_PATH === undefined) {
     THINK.RESOURCE_PATH = THINK.ROOT_PATH + '/www';
 }
-if (!isDir(THINK.RESOURCE_PATH)) {
-    mkdir(THINK.RESOURCE_PATH);
-}
+
 //应用目录
 if (THINK.APP_PATH === undefined) {
     THINK.APP_PATH = THINK.ROOT_PATH + '/App';
 }
-if (!isDir(THINK.APP_PATH)) {
-    mkdir(THINK.APP_PATH);
-}
+
 //框架目录
 if (THINK.THINK_PATH === undefined) {
     THINK.THINK_PATH = __dirname + '/lib';
@@ -64,35 +40,25 @@ if (THINK.CORE_PATH === undefined) {
 if (THINK.RUNTIME_PATH === undefined) {
     THINK.RUNTIME_PATH = THINK.ROOT_PATH + '/Runtime';
 }
-if (!isDir(THINK.RUNTIME_PATH)) {
-    mkdir(THINK.RUNTIME_PATH);
-}
+
 //日志目录
 if (THINK.LOG_PATH === undefined) {
     THINK.LOG_PATH = THINK.RUNTIME_PATH + '/Logs';
 }
-if (!isDir(THINK.LOG_PATH)) {
-    mkdir(THINK.LOG_PATH);
-}
+
 //缓存目录
 if (THINK.TEMP_PATH === undefined) {
     THINK.TEMP_PATH = THINK.RUNTIME_PATH + '/Temp';
 }
-if (!isDir(THINK.TEMP_PATH)) {
-    mkdir(THINK.TEMP_PATH);
-}
+
 if (THINK.DATA_PATH === undefined) {
     THINK.DATA_PATH = THINK.RUNTIME_PATH + '/Data';
 }
-if (!isDir(THINK.DATA_PATH)) {
-    mkdir(THINK.DATA_PATH);
-}
+
 if (THINK.CACHE_PATH === undefined) {
     THINK.CACHE_PATH = THINK.RUNTIME_PATH + '/Cache';
 }
-if (!isDir(THINK.CACHE_PATH)) {
-    mkdir(THINK.CACHE_PATH);
-}
+
 
 //线上环境自动关闭debug模式
 if (process.argv[2] === 'online') {
